afafbcca80eb0ed8b687db2f6ea282dcd1c077db
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2016-12-04 21:03:27 +0100

    Fixed webpack 1 problems

--- webpack/webpack.resolve.js
+++ webpack/webpack.resolve.js
@@ -3,5 +3,8 @@ var path = require('path');
 module.exports = {
   extensions: ['', '.js', '.jsx'],
   root: path.resolve('./src/'),
-  modulesDirectories: ['node_modules']
+  modulesDirectories: ['node_modules'],
+  alias: {
+    sortobject: path.join(__dirname, '..', 'node_modules', 'sortobject', 'es2015', 'index.js')
+  }
 };
